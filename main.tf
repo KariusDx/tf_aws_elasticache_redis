907e34b196bf4ff275bed564bbf9987f8a1e777c
Micah Frederick <mfrederick@ptp.cloud>
2023-11-06 14:38:12 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 resource "aws_elasticache_replication_group" "redis" {
   replication_group_id          = "${var.project}-${var.environment}-${var.name}"
-  replication_group_description = "Redis cluster for ${var.project}-${var.environment}-${var.name}"
+  description                   = "Redis cluster for ${var.project}-${var.environment}-${var.name}"
   engine                        = "redis"
   engine_version                = var.engine_version
   port                          = var.port
   node_type                     = var.node_type
-  number_cache_clusters         = var.num_cache_nodes
+  num_cache_clusters            = var.num_cache_nodes
   parameter_group_name          = var.parameter_group_name
   security_group_ids            = [aws_security_group.sg_redis.id]
   subnet_group_name             = aws_elasticache_subnet_group.elasticache.id
-  availability_zones            = var.availability_zones
+  preferred_cache_cluster_azs   = var.availability_zones
   automatic_failover_enabled    = var.automatic_failover_enabled
   snapshot_window               = var.snapshot_window
   snapshot_retention_limit      = var.snapshot_retention_limit
